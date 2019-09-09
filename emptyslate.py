b7ba17b840705f0f1acf4a07db6b8ca097e3b21c
JCoder121 <j.chen14641@gmail.com>
2019-09-09 10:59:08 -0700

    test file

---
+++ emptyslate.py
@@ -0,0 +1,16 @@
+#use for testing = blank canvas
+
+print('hello world')
+x = 52
+print(x+2)
+
+
+def thisisatest():
+	my_int = int(input('what do you want to input?'))
+	new_val = my_int ** 2
+	print(new_val)
+	return
+
+thisisatest()
+
+print('done')
