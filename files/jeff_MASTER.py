06c3c6e7410eccedf5a7b129fe45011ae597370f
Jeffrey Chen <j.chen14641@gmail.com>
2020-04-02 13:17:39 -0700

    Update jeff_MASTER.py

--- files/jeff_MASTER.py
+++ files/jeff_MASTER.py
@@ -2,15 +2,15 @@
 #while making more edits on most current
 #here will be the last fully working most_current.py file
 
-#updated section 3/20/20 8:10PM
+#updated section 4/2/20 10:00AM
 #anichau and jeffchen
-#signed Anirudh Chaudhary
-#Update: Sequential Car Dropoff: Cars will dropoff if the car in front of it dropped off
+#signed Jeffrey Chen
+
 
 '''Things to do:
-1. Sequential car dropping if in the dropoff zone and the car in front is also dropping off
-2. Drop off in the vertical and horizontal direction
+make function for pedestrian spawning every x seconds
 '''
+
 import pygame
 import random
 import math  
@@ -22,8 +22,7 @@ from time import gmtime, strftime
 notes:
 YELLOW CARS = HAVE DROPPED OFF ALREADY
 next step: 
-do average speed, etc. in mph
-COMMON LOCATIONS
+do average speed, etc. in mph, work on statistics and testing
 '''
 
 # Define colors
@@ -40,20 +39,52 @@ SCREEN_HEIGHT = 850
 CAR_SIZE = 15
 SPEED = 0.1
 PERSON_SPEED = 1.5
-PERSON_WALK = 200
+PERSON_WALK = 175
 MAXSPEED = 1.5
 ACCEL = 0.5
 RANDOMPARAM = 8
 INFRONT = 40
 SWEEP = 20
 
-WAITTIME = 100000
 #change dt for acceleration change, bigger = faster accel
 dt = 0.0006
 
 #set height and width of screen
 size = [SCREEN_WIDTH, SCREEN_HEIGHT]
 screen = pygame.display.set_mode(size)
+
+def obj_drop_off(obj):
+
+    if not obj.is_dropped:
+        drop_in_front = False
+
+        #If the car is in its intended drop off location and it hasn't dropped and it isn't dropping
+        if(not obj.in_progress):  
+            obj.stop(True)
+            obj.final_time = (pygame.time.get_ticks() / 1000) + obj.wait_time
+            obj.in_progress = True
+
+        current_time = pygame.time.get_ticks() / 1000
+        if current_time < obj.final_time:
+            current_time = pygame.time.get_ticks() / 1000
+            obj.stop(True)
+
+        else:
+            obj.is_dropped = False
+            
+    return
+
+#make sure cars do not spawn and overlap
+def spawn_detect():
+        car_spawn_bool = True
+        for width in range(0, 100):
+            for height in range(0, 50):
+                spawn_value = screen.get_at(( (1200 + width) , (650 + height) ))
+                if spawn_value == RED:
+                    car_spawn_bool = False
+                    break
+
+        return car_spawn_bool
 
 class Car:
     """
@@ -62,56 +93,59 @@ class Car:
     def __init__(self):
         self.x = 1250
         self.y = 700
-        #below is spawn cars close to bottom
+        #if spawning car closer, use x = 150, y = 200
         #self.x = 150
-        #self.y = 600
+        #self.y = 200
+
         self.speed = SPEED/2
         self.accel = 0
         self.color = RED
 
-        self.drop_val = -1
         self.drop_x = 0
         self.drop_y = 0
 
-        self.drop_bool = True
         self.accel_bool = True
         self.is_dropped = False
-        self.pass_bool = True
-        self.drop = False
-        self.time = random.randint(10,15)
-        self.dropping = ""
+        self.start=0
+        self.final_time =0 
+
+        #time to wait for passengers to get out
+        self.wait_time = random.randint(10,15)
+        self.in_progress = False
         self.start_time_bool = True
+       
 
-
-    def stop(self, the_drop_bool):
+    def stop(self, change_color):
         self.speed = 0
         self.accel = 0
-        if the_drop_bool:
+        if change_color:
             self.color = YELLOW        
 
-    def detect(self, detect_var):
+    def detect(self, location):
         
         for sweep in range(-SWEEP, SWEEP):
             for infront in range(20, INFRONT):
                 #right
-                if detect_var == 0:
+                if location == "right":
                     detect_value = screen.get_at((int(self.x)+sweep, int(self.y)-infront))
 
                 #top
-                elif detect_var == 1:
+                elif location == "top":
                     detect_value = screen.get_at(((int(self.x)-infront), int(self.y)+sweep))
+                    #print("seen")
 
                 #left
-                elif detect_var == 2:
+                elif location == "left":
                     detect_value = screen.get_at((int(self.x)+sweep, int(self.y)+infront)) 
 
-                if (detect_value==RED) or (detect_value==GREEN) or (detect_value==YELLOW):
-                    self.accel_bool = False
-                    self.stop(False)
-                    return (self.accel_bool)         #in the future, experiment with seeing if you can return the detect_value and self.accel_bool
+            #car detection for red, green and yellow    
+            if (detect_value==RED) or (detect_value==GREEN) or (detect_value==YELLOW):
+                self.accel_bool = False
+                self.stop(False)
+                return (self.accel_bool)   
 
 
-    def accelerate(self, var1):
+    def accelerate(self, location):
         self.ACCEL = self.accel * self.accel * dt
         self.speed = (self.speed + (self.ACCEL))
 
@@ -119,27 +153,28 @@ class Car:
             self.speed = MAXSPEED
 
             #right
-            if var1 == 0:
+            if location == "right":
                 self.y += -MAXSPEED
             #top
-            elif var1 == 1:
+            elif location == "top":
                 self.x += -MAXSPEED
-            #lieft
-            elif var1 == 2:
+            #left
+            elif location == "left":
                 self.y += MAXSPEED
 
         else:
             #right
-            if var1 == 0:
+            if location == "right":
                 self.y += -self.speed 
             #top
-            elif var1 == 1:
+            elif location == "top":
                 self.x += -self.speed
             #left
-            elif var1 == 2:
+            elif location == "left":
                 self.y += self.speed
 
         self.accel+= ACCEL
+
 
 class Person:
     """
@@ -150,12 +185,10 @@ class Person:
         self.x = 0
         self.y = 0
         self.speed = PERSON_SPEED
-        self.initial = 0
-        self.personal_value = random.randint(0,1)
-        self.person = 5
+        self.spawn_point = 0
 
-        #CHANGE THIS LATERRRR
-        #self.personal_value = 1
+        #personal_value for WHERE ped will spawn
+        self.personal_value = random.randint(0,1)
         self.start_time_bool = True
    
     def stop(self):
@@ -165,18 +198,14 @@ def make_car():
     """
     Function to make a new, random car.
     """
-    car = Car()                                                                      
-    car.drop_x = random.randint(77, 250) * 2
+    car = Car()
+    if random.randint(0,1) == 0:
+        car.drop_x = random.randint(77, 250) * 2
+    
+    else:
+        car.drop_y = random.randint(50,150) * 2
 
     return car
-
-'''
-def person_interval(time):
-
-    if car.start_time_bool:
-        start = pygame.time.get_ticks() / 1000
-        car.start_time_bool = False
-'''
 
 
 def make_person():
@@ -187,20 +216,18 @@ def make_person():
     if person.personal_value == 0:
         person.x = 800
         person.y = 230
-        person.initial = person.y
+        person.spawn_point = person.y
 
     #left side
     elif person.personal_value == 1:
         person.x = 260
-        person.initial = person.x
         person.y = 450
+        person.spawn_point = person.x
 
     return person
  
 def main():
-    '''
-    This is our main program.
-    '''
+
     pygame.init()
     pygame.display.set_caption("PHS Car Traffic")
  
@@ -215,19 +242,22 @@ def main():
     person_list = []
     car_count = 0
 
+    #make first car and person
     car = make_car()
     car_list.append(car)
-    #first_cross = make_person()
-    #person_list.append(first_cross)
+    first_cross = make_person()
+    person_list.append(first_cross)
+
+    #pedestrian spawn variables (for interval purposes)
     my_seconds = 0
     start_seconds = 0
     start_seconds_bool = True
     end_seconds = 0
     total_seconds = 0
     first_car_bool = True
-    dropping = ""
     initial_time_bool = True
-    
+    Ped_walk_bool = False
+    ped_time = False
     
  
     # -------- Main Program Loop -----------
@@ -259,28 +289,20 @@ def main():
                 person = make_person()
                 person_list.append(person)
         
-        #random car spawn
-        random_car = random.randint(1,RANDOMPARAM)
 
-        #view finding of rectangle to make sure nearest car is out of area
-        spawn_bool = True
-        for width in range(0, 100):
-            for height in range(0, 50):
-                spawn_value = screen.get_at(( (1200 + width) , (650 + height) ))
-                if spawn_value == RED:
-                    spawn_bool = False
-                    break
-
+        #view finding of rectangle to make sure nearest car is out of area to prepare for spawn
         #check if random match and there is a car within rectangular area of start
-        if (random_car==3) and spawn_bool:
-            #- len car list to restrict car number at once on screen
+        
+        if ((random.randint(1, RANDOMPARAM)) == 3) and spawn_detect():
             car = make_car()
             car_list.append(car)
+        
 
+        ped_current_time = pygame.time.get_ticks() / 1000
         
         #random pedestrian spawn TO EDIT
         person_time = int((pygame.time.get_ticks() / 1000))
-        time_mod = person_time % 5
+        time_mod = person_time % 10
         person_counter = 0
         
         person_spawn_bool = True
@@ -290,71 +312,72 @@ def main():
             if spawn_value_left == GREEN or spawn_value_top == GREEN:
                 person_spawn_bool = False
                 break
-
-        if person_spawn_bool and time_mod == 0:
-            for x in range(0,20):
-                person = make_person()
-                person_list.append(person)
-            #person_counter += 1
-
-
-        random_ped = random.randint(1, RANDOMPARAM+100)
-        #random_ped = 3
-        if (random_ped == 3):
-            pass
-            #see how this goes 
-            #person = make_person()
-            #person_list.append(person)        
+            
+        if ped_current_time % 60 > -0.1 and ped_current_time % 60 < 0.1:
+            Ped_walk_bool = True
+            print("yes")
+        
+        if Ped_walk_bool == True and ped_time == False:                         
+            # If its been 60 seconds, start timer
+            start_ped_time = pygame.time.get_ticks() / 1000
+            ped_time = True
+            print(start_ped_time)
+            print("^ is the start time")
+       
+        elif ped_time and ped_current_time < start_ped_time + 10:       
+        #Timer running and less than 10 seconds...change the 10 to be a variable
+            person = make_person()
+            person_list.append(person)
+       
+        elif ped_time and ped_current_time > start_ped_time + 10:       
+        #Timer running and its been more than 10 seconds
+            Ped_walk_bool = False
+            ped_time = False
+              
  
         # --- car logic
         for car in car_list:
             #Move the car's center, check for position to move car where
             car.accel_bool = True
             
-
             #see if car is on right side, will go vertical up
             if car.x > 155 and car.y > 100:
-                car.detect(0)
+                car.detect("right")
                 if car.accel_bool:
-                    car.accelerate(0)
-
+                    car.accelerate("right")
 
             #see if car is on top side, will go horizontal left 
-            
             elif car.x > 155 and car.y < 100:
-                detect_val = screen.get_at((int(car.x)-INFRONT, int(car.y)))
-                current_time = pygame.time.get_ticks() / 1000
+                drop_bool = (int(car.x)==car.drop_x) or (int(car.x-1) == car.drop_x)
+                drop_detect = False
+                for z in range(3, INFRONT):
+                    detect_val = screen.get_at((int(car.x-INFRONT), int(car.y)))
+                    if detect_val == YELLOW:
+                        drop_detect = True
 
-                if(((int(car.x)==car.drop_x) or (int(car.x-1)==car.drop_x)) and (car.drop == False) and (car.dropping != "In Progress")):  #If the car is in its intended drop off location and it hasn't dropped and it isn't dropping
-                   car.stop(True)
-                   car.dropping = "In Progress"
-                   print(car.dropping)
-                   if car.start_time_bool:
-                       start = pygame.time.get_ticks() / 1000
-                       car.start_time_bool = False
+                if drop_bool or (detect_val == YELLOW and (car.x > 150 and car.x < 500)):
+                    obj_drop_off(car)
                 
-                   
-                elif (detect_val == YELLOW and (car.x >154 and car.x < 500) and (car.drop == False) and (car.dropping != "In Progress")): #If the car in front is dropping and the present car is not dropping
-                   car.stop(True)
-                   car.dropping = "In Progress"         #A Consideration: If the car in front of the present car stops only slightly at a turn for example, the present car will signify that it is dropping off. As a result, it sometimes doesn't wait as long as a normal dropoff would. If it is a big deal then we can set a timer for 5 seconds or so for  mandatory waiting
-                   print("Car in front stopped")
-                   
-                
-                elif (car.dropping =="In Progress"):
-                    car.stop(True)
-                    if current_time > start + car.time:
-                        car.drop = True
-                        car.dropping = ""
-                else:
-                    car.detect(1)
-                    if car.accel_bool: 
-                        car.accelerate(1)
-                
-            elif car.x < 156 and car.y > 90:
-                #see if car is on left side, will go vertical down
-                car.detect(2)
+                car.detect("top")
                 if car.accel_bool:
-                    car.accelerate(2)
+                    car.accelerate("top")
+                
+                
+            #see if car is on left side, will go vertical down
+            elif car.x < 156 and car.y > 90:
+                drop_bool = (int(car.y)==car.drop_y) or (int(car.y-1) == car.drop_y)
+                drop_detect = False
+                for z in range(3, INFRONT):
+                    detect_val = screen.get_at((int(car.x), int(car.y) + INFRONT))
+                    if detect_val == YELLOW:
+                        drop_detect = True
+
+                if drop_bool or (drop_detect and (car.y > 150 and car.y < 350)):
+                    obj_drop_off(car)
+                
+                car.detect("left")
+                if car.accel_bool:
+                    car.accelerate("left")
 
                                         
             #get rid of car if it crosses bottom line (memory management)
@@ -364,12 +387,10 @@ def main():
                 if car_count == 1 and first_car_bool:
                     start_seconds = pygame.time.get_ticks() / 1000
                     first_car_bool = False
-                #print("cars crossed:", car_count)
+                  
                 print(str(car_count) + " cars")
                 print("Time elapsed " + str(total_seconds))
-                #cars_compare = 100
-
-                #LOOK HERE ANICHAU
+              
                 #currently, on every car pass, write time needed
                 #if writing time only for all 100 to pass, change cars_compare to 100
                 cars_compare = 130
@@ -378,26 +399,21 @@ def main():
                     done = True
 
                 #fail safe to check if car going down is still red - not dropped off = bug and need to be fixed
-                
-                '''
-                #will go through this later
                 if car.color == RED:
-                    print("car not dropped off, something wrong")
+                    print("car not dropped off, something wrong")
                     done = True
-                '''
-
+                
         #pedestrian logic
         for person in person_list:
 
             person_bool = True
-
 
             #person coming from top leg
             if person.personal_value == 0:
                 for y in range(10,20):
                     for person_sweep in range(-4, 4):
                         person_infront = screen.get_at(((int(person.x) + person_sweep), int(person.y) -y))
-                        if person_infront==RED or person_infront==GREEN or person_infront==YELLOW:
+                        if person_infront==RED or person_infront==YELLOW:
                             person.stop()
                             person_bool = False
 
@@ -410,7 +426,7 @@ def main():
                     for person_sweep in range(-4, 4):
                         person_infront = screen.get_at((int(person.x)-x, int(person.y)+person_sweep))
                         
-                        if person_infront==RED or person_infront==GREEN or person_infront==YELLOW:
+                        if person_infront==RED or person_infront==YELLOW:
                             person.stop()
                             person_bool = False
 
@@ -418,17 +434,13 @@ def main():
                     person.x -= PERSON_SPEED
 
             #always check if person has walked PERSON_WALK pixels, then erase from list (memory management)
-
             #delete from top leg
-            if((person.personal_value == 0) and (abs(person.initial - person.y) > PERSON_WALK)):
+            if((person.personal_value == 0) and (abs(person.spawn_point - person.y) > PERSON_WALK)):
                 del(person_list[0])
 
             #delete from right leg
-            elif((person.personal_value== 1) and (abs(person.initial - person.x) > PERSON_WALK)):
+            elif((person.personal_value== 1) and (abs(person.spawn_point - person.x) > PERSON_WALK)):
                 del(person_list[0])
-
-
-
 
         # --- Drawing
         # Set the screen background
@@ -438,6 +450,13 @@ def main():
         pygame.draw.rect(screen, BLACK, (100, 50, 1200, 100))
         pygame.draw.rect(screen, BLACK, (1200, 50, 100, 700))
         pygame.draw.rect(screen, BLACK, (100, 50, 100, 700))
+        
+        #cars will drop off in these zones
+        pygame.draw.rect(screen, BLUE, (500, 150, 10, 10))
+        pygame.draw.rect(screen, BLUE, (500, 40, 10, 10))
+        pygame.draw.rect(screen, BLUE, (200, 300, 10,10))
+        pygame.draw.rect(screen, BLUE, (90, 300, 10,10))
+
         
         # draw all moving objects
         
@@ -450,15 +469,10 @@ def main():
  
         # --- Wrap-up, limit to 60 frames per second
         clock.tick(60)
-        #my_seconds+=1
-        #mytime = my_seconds/60
-
         
-        #print(int(mytime))
         end_seconds = pygame.time.get_ticks() / 1000
         total_seconds = end_seconds - start_seconds
             
-        
         # update screen with newly drawn
         pygame.display.flip()
  
@@ -469,6 +483,7 @@ def main():
         print(body_print)
 
         real_time = strftime("%Y-%m-%d %H-%M-%S", gmtime())
+        #save_path = "/Users/Anirudh/Documents"
         save_path = "/Users/jeffrey/Documents/Github/carsim/files/testing_current"
     
         filestring = "rawdata- " + str(real_time) + ".txt"
